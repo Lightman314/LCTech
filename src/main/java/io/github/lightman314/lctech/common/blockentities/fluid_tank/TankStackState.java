7e93510dfa313eb9147e9317e10b7dc6638deb01
Lightman <lightman314@gmail.com>
2025-09-07 12:55:52 -0400

    0.2.2.4 Patch A

--- src/main/java/io/github/lightman314/lctech/common/blockentities/fluid_tank/TankStackState.java
+++ src/main/java/io/github/lightman314/lctech/common/blockentities/fluid_tank/TankStackState.java
@@ -3,9 +3,11 @@ package io.github.lightman314.lctech.common.blockentities.fluid_tank;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+
 public enum TankStackState implements StringRepresentable { SOLO, BOTTOM, MIDDLE, TOP;
     @Override
-    public @NotNull String getSerializedName() { return this.name().toLowerCase(); }
+    public @NotNull String getSerializedName() { return this.name().toLowerCase(Locale.ENGLISH); }
     public final boolean isSolo() { return this == SOLO; }
     public final boolean isBottom() { return this == BOTTOM; }
     public final boolean isTop() { return this == TOP; }
